23af1ef109c502fd602d2a5edd46360be34cb470
Gordon Dexter <gordon@texasdex.com>
2015-02-17 23:50:24 -0500

    Improvements to arg parsing and flexibility

--- joyvol.py
+++ joyvol.py
@@ -5,24 +5,45 @@ import daemon
 import time
 import pynotify
 import os
+import argparse
+
 
 def main():
-  pygame.init()
-  pygame.joystick.init()
-  js = pygame.joystick.Joystick(0)
-  js.init()
-  pynotify.init("Joyvol")
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.JOYBUTTONDOWN:
-        pynotify.Notification("UNMUTE", "Headphones stowed, unmuting speakers" ).show()
-        os.system("amixer -q set 'Surround',0 100")
-#        print "UNMUTE"
-      if event.type == pygame.JOYBUTTONUP:
-        pynotify.Notification("MUTE", "Headphones in use, muting speakers" ).show()
-        os.system("amixer -q set 'Surround',0 0")
-#        print "MUTE"
-    time.sleep(.5)
+    parser = argparse.ArgumentParser(description='JoyVol: control audio (or anything else really) based on joystick buttons')
+    parser.add_argument('--js', dest='joystick_id', action='store_const',
+                        const=sum, default=0,
+                        help='the pygame ID of the joystick')
+    parser.add_argument('--button', dest='button_id', action='store_const',
+                        const=sum, default=0,
+                        help='the pygame ID of the button')
+    parser.add_argument('--libnotify', dest='libnotify', action='store_true')
+    parser.add_argument('--no-libnotify', dest='libnotify', action='store_false')
+    parser.set_defaults(libnotify=True)
+
+
+    args = vars(parser.parse_args())
+
+    pygame.init()
+    pygame.joystick.init()
+    js = pygame.joystick.Joystick(0)
+    js.init()
+    pynotify.init("Joyvol")
+    while True:
+        for event in pygame.event.get():
+            if (event.type == pygame.JOYBUTTONDOWN and
+               event.dict['joy'] == args['joystick_id'] and
+               event.dict['button'] == args['button_id']):
+                os.system("amixer -q set 'Surround',0 100")
+                if args['libnotify']:
+                    pynotify.Notification("UNMUTE", "Headphones stowed, unmuting speakers" ).show()
+            if (event.type == pygame.JOYBUTTONUP and
+               event.dict['joy'] == args['joystick_id'] and
+               event.dict['button'] == args['button_id']):
+                os.system("amixer -q set 'Surround',0 0")
+            	if args['libnotify']:
+                	pynotify.Notification("MUTE", "Headphones in use, muting speakers" ).show()
+
+        time.sleep(.5)
 
 
 #with daemon.DaemonContext():
